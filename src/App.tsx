ba4397b46c41ba956f6d14716e435a9665a2b7c2
hoangtriet <hoangtriet@hoangtriets-MacBook-Pro.local>
2022-02-01 01:20:50 +0700

    sample login with metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from './Component/Layout';
+import ConnectButton from './Component/ConnectButton';
+import AccountModal from './Component/AccountModal';
+import { useDisclosure } from '@chakra-ui/react';
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+      <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Layout>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DAppProvider } from '@usedapp/core';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={{}}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
